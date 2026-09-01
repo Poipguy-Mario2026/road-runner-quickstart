e49e345f263a198e02de5a6e857043adf632bdb1
Poipguy <304970214+Poipguy-Mario2026@users.noreply.github.com>
2026-09-01 17:57:19 -0400

    Mario_Intake_and_Drive
    Mecanum and Intake Code. This is for Mario Express

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpMode.java
@@ -5,13 +5,21 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="TeleOp_Main")
 public class MainTeleOpMode extends LinearOpMode {
     private MecanumDrive drive;
+    public DcMotor IntakeMotor;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        IntakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
+        IntakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
 
         while (opModeIsActive()) {
@@ -25,6 +33,19 @@ public class MainTeleOpMode extends LinearOpMode {
             PoseVelocity2d velocity = new PoseVelocity2d(translationalVelocity, rotationalVelocity);
 
             drive.setDrivePowers(velocity);
+
+            //Intake Code
+            if(gamepad1.a == true){
+                IntakeMotor.setPower(1);
+
+            } else {
+                IntakeMotor.setPower(0);
+            }
+
+
+
+
+
 
             telemetry.update();
 
